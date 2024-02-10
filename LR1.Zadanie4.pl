f2228c564f8ea3eea4b299b58193fa73d63eeaf3
MilenNastya <s0172018@edu.kubsu.ru>
2024-02-10 21:56:32 +0300

    Сделано задание 4( АКИНАТОР 1).

---
+++ LR1.Zadanie4.pl
@@ -0,0 +1,142 @@
+main :-
+    retractall(asked(_,_)),
+    fault(Problem),
+    !,
+    nl,
+    write('The problem is '), write(Problem), write(.), nl.
+main :-
+    nl,
+    write('The problem cannot be recognized.'), nl.
+
+problem(disc_format) :-
+    query('Does the computer show error cannot format').
+
+problem(boot_failure) :-
+    query('Does the computer show boot failure').
+
+problem(bad_sector) :-
+    query('Does the computer show bad sector error').
+
+problem(cannot_read) :-
+    query('Does the computer show cannot read from specified device').
+
+problem(long_beep) :-
+    query('Is there a long beep during bootup').
+
+problem(short_beep) :-
+    query('Is there a short beep during bootup').
+
+problem(two_long_beeps) :-
+    query('Are there two long beeps during bootup').
+
+problem(two_short_beeps) :-
+    query('Are there two short beeps during bootup').
+
+problem(blank_display) :-
+    query('Is there a blank display during bootup').
+
+problem(repeating_short_beeps) :-
+    query('Are there repeating short beeps during bootup').
+
+problem(continuous_beeps) :-
+    query('Is there a continuous beep during bootup').
+
+problem(no_beep) :-
+    query('Is there a beep during bootup').
+
+problem(not_printing) :-
+    query('Is there a problem with printing').
+
+problem(missing_dots) :-
+    query('Is there a missing character during printing').
+
+problem(non_uniform_printing) :-
+    query('Is there uniform printing').
+
+problem(spread_ink) :-
+    query('Is there spreading of ink during printing').
+
+problem(paper_jam) :-
+    query('Is there a paper jam during printing').
+
+problem(out_of_paper) :-
+    query('Is there out-of- paper error during printing').
+%Новые проблемы.
+problem(click):-
+    query('If you click on it, will it sound like a click?').
+problem(BSOD) :-
+    query('Does it store, retrieve and process data?').
+problem(large_size):-
+    query('Is the problem due to the large size?').
+
+problem(does_not_freeze):-
+    query('Defrosted').
+
+problem(oval_view):-
+    query('Has this object bent and turned into an oval?').
+
+problem(its_burning_up):-
+    query('Is the food starting to burn to her?').
+% Добавлен объект 1 - без доп вопросов.
+%Добавить несколько объектов таким образом, чтобы их можно было однозначно определить без введения дополнительных вопросов.
+fault(the_mouse):- problem(click).
+fault(processor) :- problem(BSOD).
+%с введением дополнительных вопросов.
+fault(fridge):-
+    problem(large_size),
+    problem(does_not_freeze).
+fault(frying_pan):-
+    problem(oval_view),
+    problem(its_burning_up).
+
+fault(power_supply) :-
+    problem(repeating_short_beeps),
+    problem(continuous_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(display_adapter) :-
+    problem(long_beep),
+    problem(two_short_beeps),
+    problem(blank_display),
+    problem(no_beep).
+
+fault(motherboard) :-
+    problem(long_beep),
+    problem(short_beep).
+
+fault(hard_disc) :-
+    problem(two_short_beeps),
+    problem(blank_display).
+
+fault(booting_problem) :-
+    problem(bad_sector),
+    problem(boot_failure).
+
+fault(floppy_disk_unusable) :-
+    problem(bad_sector),
+    problem(cannot_read),
+    problem(disc_format).
+
+fault(printer_head) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(nonuniform_printing).
+
+fault(ribbon) :-
+    problem(not_printing),
+    problem(missing_dots),
+    problem(spread_ink).
+
+fault(paper) :-
+    problem(not_printing),
+    problem(paper_jam),
+    problem(out_of_paper).
+
+query(Prompt) :-
+    (   asked(Prompt, Reply) -> true
+    ;   nl, write(Prompt), write(' (y/n)? '),
+        read(X),(X = y -> Reply = y ; Reply = n),
+	assert(asked(Prompt, Reply))
+    ),
+    Reply = y.
