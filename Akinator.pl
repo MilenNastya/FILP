6d7e6f9b883d242d48cf26136cffec10f0ec6e9a
MilenNastya <s0172018@edu.kubsu.ru>
2024-03-08 01:57:12 +0300

    Изменён  акинатор на предметную область iPhone. Добавлены вопросы, объекты и вопрос-ответная система

--- Akinator.pl
+++ Akinator.pl
@@ -8,130 +8,141 @@ main :-
     nl,
     write('The problem cannot be recognized.'), nl.
 
-problem(disc_format) :-
-    query('Does the computer show error cannot format').
+problem("Самый маленький"):-query('Диагональ устройства равна 3.5 дюйма?').
+problem("Маленький"):-query('Диагональ устройства равна 4.0 дюйма?').
+problem("Между маленьким и средним"):-query('Диагональ устройства равна 4,7-5,4 дюйма?').
+problem("Между средним и маленьким"):-query('Диагональ устройства равна 5,5 -5.7 дюйма?').
+problem("Cредний"):-query('Диагональ устройства равна 5,8-6.1 дюйма').
+problem("Большой"):-query('Диагональ устройства равна 6.5 -6.7?').
 
-problem(boot_failure) :-
-    query('Does the computer show boot failure').
+problem("Процессор для 3 айфона"):-query('Процессор устройства Samsung APL0298C05?').
+problem("Процессор для 4 айфона"):-query('Процессор устройства Apple A4?').
+problem("Процессор для 5 айфона"):-query('Процессор устройства pple A6?').
+problem("Процессор для 5s айфона"):-query('Процессор устройства Apple A7?').
+problem("Процессор для 6 айфона"):-query('Процессор устройства Apple A8?').
+problem("Процессор для 6s айфона"):-query('Процессор устройства Apple A9?').
+problem("Процессор для 7/7plus айфона"):-query('Процессор устройства Apple A10 Fusion?').
+problem("Процессор для 8/8 plus/X айфона"):-query('Процессор устройства Apple A11 Bionic?').
+problem("Процессор для XR айфона"):-query('Процессор устройства Apple A12 Bionic?').
+problem("Процессор для 11 айфона"):-query('Процессор устройства Apple A13 Bionic?').
+problem("Процессор для 12 айфона"):-query('Процессор устройства Apple A14 Bionic?').
+problem("Процессор для 13 айфона"):-query('Процессор устройства Apple A15 Bionic?').
+problem("Процессор для 14 айфона"):-query('Процессор устройства Apple A16 Bionic?').
+problem("Процессор для 15 айфона"):-query('Процессор устройства Apple A17 Bionic?').
 
-problem(bad_sector) :-
-    query('Does the computer show bad sector error').
+problem("Новшества 3"):-query('У вашего устройства только появилась поддержка более быстрого сетевого стандарта 3G?').
+problem("Новшества 4"):-query('У вашего устройства только появилась фронтальная камера?').
+problem("Новшества 5"):-query('У вашего устройства только появился 4-ч дюймовый дисплей?').
+problem("Новшества 5s"):-query('У вашего устройства только появилась память на 64 гб?').
+problem("Новшества 6"):-query('У вашего устройства только появилась память на 128 гб?').
+problem("Новшества 7"):-query('У вашего устройства только появилась возможность съемки на широкоугольную камеру?').
+problem("Новшества 8"):-query('У вашего устройства только появился True Tone Display?').
+problem("Новшества X"):-query('У вашего устройства только появилась "чёлка"?').
+problem("Новшества XR"):-query('У вашего устройства такие же почти характеристики, как у X, но он дешевле?').
+problem("Новшества 11"):-query('У вашего устройства только появилась возможность ночной съемки?').
+problem("Новшества 12"):-query('У вашего устройства только появилась возможность снимать видео/фотографии с 5-кратным оптическим зумом?').
+problem("Новшества 13"):-query('У вашего устройства только появилась частота обновления экранов -120 гц? ').
+problem("Новшества 14"):-query('У вашего устройства такие же характеристики, как у 13?').
+problem("Новшества 14 PRO MAX"):-query('У вашего устройства только появился Dynamic Island? ').
+problem("Новшества 15"):-query('У вашего устройства поменялся разъем для зарядки?').
+problem("Новшества 15 PRO MAX"):-query('Ваше устройство находится в титановом корпусе?').
 
-problem(cannot_read) :-
-    query('Does the computer show cannot read from specified device').
+fault('Iphone 3') :-
+    problem("Самый маленький"),
+    problem("Процессор для 3 айфона"),
+    problem("Новшества 3").
 
-problem(long_beep) :-
-    query('Is there a long beep during bootup').
+fault('Iphone 4') :-
+    problem("Самый маленький"),
+    problem("Процессор для 4 айфона"),
+    problem("Новшества 4").
 
-problem(short_beep) :-
-    query('Is there a short beep during bootup').
+fault('Iphone 5') :-
+    problem("Маленький"),
+    problem("Процессор для 5 айфона"),
+    problem("Новшества 5").
 
-problem(two_long_beeps) :-
-    query('Are there two long beeps during bootup').
+fault('Iphone 5s') :-
+    problem("Маленький"),
+    problem("Процессор для 5s айфона"),
+    problem("Новшества 5s").
 
-problem(two_short_beeps) :-
-    query('Are there two short beeps during bootup').
+fault('Iphone 6') :-
+    problem("Между маленьким и средним"),
+    problem("Процессор для 6 айфона"),
+    problem("Новшества 6s").
 
-problem(blank_display) :-
-    query('Is there a blank display during bootup').
+fault('Iphone 7') :-
+    problem("Между маленьким и средним"),
+    problem("Процессор для 7/7plus айфона"),
+    problem("Новшества 7").
 
-problem(repeating_short_beeps) :-
-    query('Are there repeating short beeps during bootup').
+fault('Iphone 7 plus') :-
+    problem("Между средним и маленьким"),
+    problem("Процессор для 7/7plus айфона"),
+    problem("Новшества 7").
 
-problem(continuous_beeps) :-
-    query('Is there a continuous beep during bootup').
+fault('Iphone 8') :-
+    problem("Между маленьким и среднем"),
+    problem("Процессор для 8/8plus/X айфона"),
+    problem("Новшества 8").
 
-problem(no_beep) :-
-    query('Is there a beep during bootup').
+fault('Iphone 8 plus') :-
+    problem("Между средним и маленьким"),
+    problem("Процессор для 8/8plus/X айфона"),
+    problem("Новшества 8").
 
-problem(not_printing) :-
-    query('Is there a problem with printing').
+fault('Iphone X') :-
+    problem("Средний"),
+    problem("Процессор для 8/8plus/10 айфона"),
+    problem("Новшества X").
 
-problem(missing_dots) :-
-    query('Is there a missing character during printing').
+fault('Iphone XR') :-
+    problem("Средний"),
+    problem("Процессор для XR айфона"),
+    problem("Новшества XR").
 
-problem(non_uniform_printing) :-
-    query('Is there uniform printing').
+fault('Iphone 11') :-
+    problem("Средний"),
+    problem("Процессор для 11 айфона"),
+    problem("Новшества 11").
 
-problem(spread_ink) :-
-    query('Is there spreading of ink during printing').
+fault('Iphone 12') :-
+    problem("Средний"),
+    problem("Процессор для 12 айфона"),
+    problem("Новшества 12").
 
-problem(paper_jam) :-
-    query('Is there a paper jam during printing').
+fault('Iphone 13') :-
+    problem("Средний"),
+    problem("Процессор для 13 айфона"),
+    problem("Новшества 13").
 
-problem(out_of_paper) :-
-    query('Is there out-of- paper error during printing').
-%Новые проблемы.
-problem(click):-
-    query('If you click on it, will it sound like a click?').
-problem(bsod) :-
-    query('Does it store, retrieve and process data?').
-problem(large_size):-
-    query('Is the problem due to the large size?').
+fault('Iphone 13 PRO MAX') :-
+    problem("Большой"),
+    problem("Процессор для 13 айфона"),
+    problem("Новшества 13").
 
-problem(does_not_freeze):-
-    query('Defrosted').
+fault('Iphone 14') :-
+    problem("Средний"),
+    problem("Процессор для 14 айфона"),
+    problem("Новшества 14").
 
-problem(oval_view):-
-    query('Has this object bent and turned into an oval?').
+fault('Iphone 14 PRO MAX') :-
+    problem("Большой"),
+    problem("Процессор для 14 айфона"),
+    problem("Новшества 14 PRO MAX ").
 
-problem(its_burning_up):-
-    query('Is the food starting to burn to her?').
-% Добавлен объект 1 - без доп вопросов.
-%Добавить несколько объектов таким образом, чтобы их можно было однозначно определить без введения дополнительных вопросов.
-fault(the_mouse):- problem(click).
-fault(processor) :- problem(bsod).
-%с введением дополнительных вопросов.
-fault(fridge):-
-    problem(large_size),
-    problem(does_not_freeze).
-fault(frying_pan):-
-    problem(oval_view),
-    problem(its_burning_up).
 
-fault(power_supply) :-
-    problem(repeating_short_beeps),
-    problem(continuous_beeps),
-    problem(blank_display),
-    problem(no_beep).
+fault('Iphone 15') :-
+    problem("Средний"),
+    problem("Процессор для 15 айфона"),
+    problem("Новшества 15").
 
-fault(display_adapter) :-
-    problem(long_beep),
-    problem(two_short_beeps),
-    problem(blank_display),
-    problem(no_beep).
+fault('Iphone 15 PRO MAX') :-
+    problem("Большой"),
+    problem("Процессор для 15 айфона"),
+    problem("Новшества 15 PRO MAX").
 
-fault(motherboard) :-
-    problem(long_beep),
-    problem(short_beep).
-
-fault(hard_disc) :-
-    problem(two_short_beeps),
-    problem(blank_display).
-
-fault(booting_problem) :-
-    problem(bad_sector),
-    problem(boot_failure).
-
-fault(floppy_disk_unusable) :-
-    problem(bad_sector),
-    problem(cannot_read),
-    problem(disc_format).
-
-fault(printer_head) :-
-    problem(not_printing),
-    problem(missing_dots),
-    problem(nonuniform_printing).
-
-fault(ribbon) :-
-    problem(not_printing),
-    problem(missing_dots),
-    problem(spread_ink).
-
-fault(paper) :-
-    problem(not_printing),
-    problem(paper_jam),
-    problem(out_of_paper).
 
 query(Prompt) :-
     (   asked(Prompt, Reply) -> true
